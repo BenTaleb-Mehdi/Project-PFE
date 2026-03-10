8bca949d45a5b3b08121368311c67e2c1410ffe3
Mehdy <mehdibentaleb548@gmail.com>
2026-03-10 12:12:46 +0000

    feat: Implement database seeders with initial CSV data for core application entities and add a class diagram.

--- PFE-Project/database/seeders/DatabaseSeeder.php
+++ PFE-Project/database/seeders/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\DB;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -15,11 +17,16 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        $this->call([
+            UserSeeder::class,
+            StaffSeeder::class,
+            ClientSeeder::class,
+            MealCategorySeeder::class,
+            MealSeeder::class,
+            ProgramSeeder::class,
+            EvolutionSeeder::class,
+            ProgramItemSeeder::class,
+            PaymentSeeder::class,
         ]);
     }
 }
